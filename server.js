28d0719bc6ba480a0800f0b24b55506694f498cb
John Wu <john.wu@mail.mcgill.ca>
2018-07-22 14:26:19 -0700

    Adding button component'

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ const app = express().use(body_parser.json())
 
 app.listen(process.env.PORT || 5000, () => console.log('Webhook is listening on port 5000'));
 
+app.get('/test', (req,res) => {
+    res.status(200).send('Server working')
+});
+
+app.get('/test-response', (req,res) => {
+  let response;
+  response = {
+    "text": `This is a manual test response`
+  }
+  callSendAPI("2040864105944817", response)  
+  res.status(200).send('Response sent')
+});
+
 /**
  * Incoming webhook from facebook page 
  */
@@ -31,6 +44,31 @@ app.post('/webhook', (req, res) => {
   }
 })
 
+/**
+ * Webhook to trigger the response dialog in slack
+ */
+app.post('/trigger-response-dialog', (req, res) => {  
+  let body = req.body;
+  if (body.object === 'page') {
+    body.entry.forEach(function(entry) {
+      let webhook_event = entry.messaging[0]
+      console.log(webhook_event)
+      let sender_psid = webhook_event.sender.id
+      console.log('Sender ID: ' + sender_psid)
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message)      
+      }  
+    })
+    res.status(200).send('EVENT_RECEIVED')
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404)
+  }
+})
+
+/**
+ * Verification endpoint for facebook
+ */
 app.get('/webhook', (req, res) => {
   const VERIFY_TOKEN = "implementai"
   let mode = req.query['hub.mode']
@@ -48,31 +86,26 @@ app.get('/webhook', (req, res) => {
   }
 })
 
-app.get('/test', (req,res) => {
-    res.status(200).send('Server working')
-});
-
-app.get('/test-response', (req,res) => {
-  let response;
-  response = {
-    "text": `This is a manual test response`
-  }
-  callSendAPI("2040864105944817", response)  
-  res.status(200).send('Response sent')
-});
-
+/**
+ * Handles incoming message from facebook page
+ * 
+ * @param sender_psid 
+ * @param received_message 
+ */
 function handleMessage(sender_psid, received_message) {
   let response;
   if (received_message.text) {
+    // retrieve user information 
     request({
       "uri": "https://graph.facebook.com/" + sender_psid + "?fields=first_name,last_name,profile_pic&access_token=" + PAGE_ACCESS_TOKEN,
       "method": "GET"
     }, (err, res, body) => {
       if (!err) {
-        response = {
+        // send automated greeting back to user immediately
+        callSendAPI(sender_psid, {
           "text": `Hi! Thank you for sending us a message. We will respond to you within 24 hours.`
-        }
-        callSendAPI(sender_psid, response)    
+        })    
+        // post the message to slack channel
         postToSlack(sender_psid, received_message.text, JSON.parse(res.body))
       } else {
         console.error("Error occurred retrieving user info:" + err)
@@ -81,6 +114,12 @@ function handleMessage(sender_psid, received_message) {
   }
 }
 
+/**
+ * Calls API to send message back to specified user
+ * 
+ * @param sender_psid 
+ * @param response 
+ */
 function callSendAPI(sender_psid, response) {
   let request_body = {
     "recipient": {
@@ -102,10 +141,6 @@ function callSendAPI(sender_psid, response) {
   }) 
 }
 
-function getSenderName(sender_psid) {
-
-}
-
 /**
  * Posts the message to the slack channel
  * 
@@ -125,10 +160,27 @@ function postToSlack(sender_psid, message, sender_info) {
         'text': 'New message recieved from McGill AI Society Facebook Page!',
         'attachments': [
             {
-                'title': sender_info.first_name + ' ' + sender_info.last_name,
-                'text': message,
-                'author_name': ' (Sender PSID: ' + sender_psid + ')',
-                'image_url': sender_info.profile_pic
+                'image_url': sender_info.profile_pic,
+                'fields': [
+                  {
+                    "title": sender_info.first_name + ' ' + sender_info.last_name,
+                    "value": message,
+                    "short": false
+                  },
+                  {
+                      "title": "Sender PSID",
+                      "value": sender_psid,
+                      "short": true
+                  }
+                ],
+                "actions": [
+                  {
+                      "name": "respond",
+                      "text": "Reply To Sender",
+                      "type": "button",
+                      "style": "primary"
+                  }
+                ]
             }
         ]
     }
