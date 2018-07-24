80d3eae373967e489745c0703377787410aaf351
John Wu <john.wu@mail.mcgill.ca>
2018-07-23 23:42:55 -0700

    Refactoring code

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 'use strict';
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const SLACK_MESSENGER_WEBHOOK = process.env.SLACK_MESSENGER_WEBHOOK;
+const AUTO_REPLY_MESSAGE = 'Hi! Thank you for sending us a message. Our neural networks are trying their best to effectively answer your questions. They’re currently training, but you should have a response within 24 hours.'
+const FACEBOOK_PAGE_NAME = 'McGill AI Society'
 
 const request = require('request')
 const express = require('express')
 const bodyParser = require('body-parser')
+
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.listen(process.env.PORT || 5000, () => console.log('Webhook is listening on port 5000'));
 
+/**
+ * Test endpoint for server
+ */
 app.get('/test', (req,res) => {
     res.status(200).send('Server working')
-});
-
-app.get('/test-response', (req,res) => {
-  let response;
-  response = {
-    "text": `This is a manual test response`
-  }
-  callSendAPI("2040864105944817", response)  
-  res.status(200).send('Response sent')
 });
 
 /**
@@ -32,11 +28,9 @@ app.post('/webhook', (req, res) => {
   if (body.object === 'page') {
     body.entry.forEach(function(entry) {
       let webhook_event = entry.messaging[0]
-      console.log(webhook_event)
       let sender_psid = webhook_event.sender.id
-      console.log('Sender ID: ' + sender_psid)
       if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message)      
+        handleFacebookMessage(sender_psid, webhook_event.message)      
       }  
     })
     res.status(200).send('EVENT_RECEIVED')
@@ -44,32 +38,6 @@ app.post('/webhook', (req, res) => {
     // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404)
   }
-})
-
-/**
- * Webhook to trigger the response dialog in slack
- */
-app.post('/messenger-reply', (req, res) => {  
-  var payload = req.body
-  var text = [].concat.apply([], payload.text.split('"').map(function(v,i){
-    return i%2 ? v : v.split(' ')
-  })).filter(Boolean);
-  // retrieve user information 
-  request({
-    "uri": "https://graph.facebook.com/" + text[0] + "?fields=first_name,last_name,profile_pic&access_token=" + PAGE_ACCESS_TOKEN,
-    "method": "GET"
-  }, (err, res, body) => {
-    if (!err) {
-      callSendAPI(text[0], {
-        "text": text[1]
-      })
-      var user_info = JSON.parse(res.body)
-      postResponseSlackNotification('@' + payload.user_name, user_info.first_name + ' ' + user_info.last_name, text[1])
-    } else {
-      console.error("Error occurred retrieving user info:" + err)
-    }
-  }) 
-  res.status(200).send("Message sent!")
 })
 
 /**
@@ -92,167 +60,156 @@ app.get('/webhook', (req, res) => {
   }
 })
 
+
+/**
+ * Webhook to trigger the response dialog in slack
+ */
+app.post('/messenger-reply', (req, res) => {  
+  var payload = req.body
+  // parse input text 
+  var text = [].concat.apply([], payload.text.split('"').map(function(v,i){
+    return i%2 ? v : v.split(' ')
+  })).filter(Boolean);
+  // retrieve user information 
+  request({
+    'uri': 'https://graph.facebook.com/' + text[0] + '?fields=first_name,last_name,profile_pic&access_token=' + PAGE_ACCESS_TOKEN,
+    'method': 'GET'
+  }, (err, res, body) => {
+    if (!err) {
+      sendFacebookMessage(text[0], {
+        'text': text[1]
+      })
+      var user_info = JSON.parse(res.body)
+      postToSlack({
+        'text': 'A response has been sent to a facebook messenger conversation.',
+        'attachments': [
+            {
+                'fields': [
+                  {
+                    'title': 'Responder',
+                    'value': '@' + payload.user_name,
+                    'short': true
+                  },
+                  {
+                    'title': 'Recipient',
+                    'value': user_info.first_name + ' ' + user_info.last_name,
+                    'short': true
+                  },
+                  {
+                    'title': 'Message',
+                    'value': text[1],
+                    'short': false
+                  }
+                ]
+            }
+        ]
+      })
+      res.status(200).send('Message sent!')
+    } else {
+      console.error('Error occurred retrieving user info: ' + err)
+      res.status(500).send('An error occurred while sending message.')
+    }
+  }) 
+})
+
 /**
  * Handles incoming message from facebook page
  * 
  * @param sender_psid 
  * @param received_message 
  */
-function handleMessage(sender_psid, received_message) {
-  let response;
-  console.log(received_message)
+function handleFacebookMessage(sender_psid, received_message) {
   if (received_message.text) {
     // retrieve user information 
     request({
-      "uri": "https://graph.facebook.com/" + sender_psid + "?fields=first_name,last_name,profile_pic&access_token=" + PAGE_ACCESS_TOKEN,
-      "method": "GET"
+      'uri': 'https://graph.facebook.com/' + sender_psid + '?fields=first_name,last_name,profile_pic&access_token=' + PAGE_ACCESS_TOKEN,
+      'method': 'GET'
     }, (err, res, body) => {
       if (!err) {
         // send automated greeting back to user immediately
-        callSendAPI(sender_psid, {
-          "text": `Hi! Thank you for sending us a message. Our neural networks are trying their best to effectively answer your questions. They’re currently training, but you should have a response within 24 hours.`
+        sendFacebookMessage(sender_psid, {
+          'text': AUTO_REPLY_MESSAGE
         })    
-        // post the message to slack channel
-        postToSlack(sender_psid, received_message.text, JSON.parse(res.body))
+        // post message to slack
+        var sender_info = JSON.parse(res.body)
+        postToSlack({
+          'text': 'New message recieved from the ' + FACEBOOK_PAGE_NAME + ' Facebook Page!',
+          'attachments': [
+              {
+                  'image_url': sender_info.profile_pic,
+                  'callback_id': 'reply_message',
+                  'fields': [
+                    {
+                      "title": 'Message',
+                      "value": received_message.text,
+                      "short": false
+                    },
+                    {
+                      'title': 'From',
+                      'value': sender_info.first_name + ' ' + sender_info.last_name,
+                      'short': true
+                    },
+                    {
+                      'title': 'Sender PSID',
+                      'value': sender_psid,
+                      'short': true
+                    }
+                  ]
+              }
+          ]
+        })
       } else {
-        console.error("Error occurred retrieving user info:" + err)
+        console.error('Error occurred retrieving sender info: ' + err)
       }
     }) 
   }
 }
 
-app.get('/test-send', (req, res) => {
-  callSendAPI('1824707100927777', {
-    "text": `Hello test`
-  })  
-  res.send(200)
-})
-
 /**
- * Calls API to send message back to specified user
+ * Sends Facebook message to a user in messenger
  * 
- * @param sender_psid 
+ * @param psid 
  * @param response 
  */
-function callSendAPI(sender_psid, response) {
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
+function sendFacebookMessage(psid, response) {
+  let request = {
+    'recipient': {
+      'id': psid
     },
-    "message": response
+    'message"': request
   }
   request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
+    'uri': 'https://graph.facebook.com/v2.6/me/messages',
+    'qs': { 'access_token': PAGE_ACCESS_TOKEN },
+    'method': 'POST',
+    'json': request_body
   }, (err, res, body) => {
     if (!err) {
-      console.log('message sent!')
+      console.log('Message sent to facebook with user PSID: ' + psid)
     } else {
-      console.error("Unable to send message:" + err)
+      console.error('Unable to send message: ' + err)
     }
   }) 
 }
 
-
 /**
- * Post to slack when exec responds to a message
- * 
- * @param responder 
- * @param recipient 
- * @param message 
+ * Posts a message to slack
  */
-function postResponseSlackNotification(responder, recipient, message) {
-  // Configure the request for sending to slack
-  var options = {
-    url: SLACK_MESSENGER_WEBHOOK,
-    method: 'POST',
-    headers: {
-        'User-Agent':       'Super Agent/0.0.1',
-        'Content-Type':     'application/json'
-    },
-    json: {
-        'text': 'Response sent to facebook messenger conversation.',
-        'attachments': [
-            {
-                'fields': [
-                  {
-                    "title": 'Responder',
-                    "value": responder,
-                    "short": true
-                  },
-                  {
-                    "title": 'Recipient',
-                    "value": recipient,
-                    "short": true
-                  },
-                  {
-                    "title": 'Message',
-                    "value": message,
-                    "short": false
-                  }
-                ]
-            }
-        ]
+function postToSlack(payload) {
+    // Configure the request for sending to slack
+    var options = {
+      url: SLACK_MESSENGER_WEBHOOK,
+      method: 'POST',
+      headers: {
+          'User-Agent':       'Super Agent/0.0.1',
+          'Content-Type':     'application/json'
+      },
+      json: payload
     }
-  }
-  // Process the request
-  request(options, function (error, response, body) {
-      if (error) {
-          console.log(error)
-      }
-      console.log("Sent successfully")
-  })
-}
-
-
-/**
- * Posts the message to the slack channel
- * 
- * @param sender_psid 
- * @param message 
- */
-function postToSlack(sender_psid, message, sender_info) {
-  // Configure the request for sending to slack
-  var options = {
-    url: SLACK_MESSENGER_WEBHOOK,
-    method: 'POST',
-    headers: {
-        'User-Agent':       'Super Agent/0.0.1',
-        'Content-Type':     'application/json'
-    },
-    json: {
-        'text': 'New message recieved from McGill AI Society Facebook Page!',
-        'attachments': [
-            {
-                'image_url': sender_info.profile_pic,
-                'callback_id': 'reply_message',
-                'fields': [
-                  {
-                    "title": 'Message',
-                    "value": message,
-                    "short": false
-                  },
-                  {
-                    "title": 'From',
-                    "value": sender_info.first_name + ' ' + sender_info.last_name,
-                    "short": true
-                  },
-                  {
-                    "title": 'Sender PSID',
-                    "value": sender_psid,
-                    "short": true
-                  }
-                ]
-            }
-        ]
-    }
-  }
-  // Process the request
-  request(options, function (error, response, body) {
-      if (error) {
-          console.log(error)
-      }
-  })
+    // Process the request
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log(error)
+        }
+    })
 }
